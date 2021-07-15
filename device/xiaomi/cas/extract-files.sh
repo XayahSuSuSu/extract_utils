32f2b5f66fa2df569cc57c227ccd8eac328bfd81
Xayah <1249475336@qq.com>
2021-07-15 19:59:18 +0800

    feat: Update scripts

--- device/xiaomi/cas/extract-files.sh
+++ device/xiaomi/cas/extract-files.sh
@@ -6,74 +6,54 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-set -e
-
-DEVICE=cas
-VENDOR=xiaomi
-
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
-
-ANDROID_ROOT="${MY_DIR}/../../.."
-
-HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
-if [ ! -f "${HELPER}" ]; then
-    echo "Unable to find helper script at ${HELPER}"
-    exit 1
-fi
-source "${HELPER}"
-
-# Default to sanitizing the vendor folder before extraction
-CLEAN_VENDOR=true
-
-KANG=
-SECTION=
-
-while [ "${#}" -gt 0 ]; do
-    case "${1}" in
-        -n | --no-cleanup )
-                CLEAN_VENDOR=false
-                ;;
-        -k | --kang )
-                KANG="--kang"
-                ;;
-        -s | --section )
-                SECTION="${2}"; shift
-                CLEAN_VENDOR=false
-                ;;
-        * )
-                SRC="${1}"
-                ;;
-    esac
-    shift
-done
-
-if [ -z "${SRC}" ]; then
-    SRC="adb"
-fi
-
 function blob_fixup() {
     case "${1}" in
-        vendor/lib/libsample1.so)
-            sed -i 's|/data/misc/sample1|/data/misc/sample2|g' "${2}"
-            ;;
-        vendor/lib64/libsample2.so)
-            "${PATCHELF}" --remove-needed "libsample3.so" "${2}"
-            "${PATCHELF}" --add-needed "libsample4.so" "${2}"
-            ;;
-        vendor/lib/libsample5.so)
-            "${PATCHELF}" --replace-needed "libsample6.so" "libsample7.so" "${2}"
-            ;;
-        vendor/lib/libsample7.so)
-            "${PATCHELF}" --set-soname "libsample7.so" "${2}"
+        vendor/lib/hw/audio.primary.kona.so)
+            # Before
+            # 07 ad 01 eb  # bl         __android_log_print
+            # 52 b0 01 eb  # bl         send_haptic_data_to_xlog
+            # After
+            # 07 ad 01 eb  # bl         __android_log_print
+            # 00 f0 20 e3  # nop
+            sed -i 's|\x07\xad\x01\xeb\x52\xb0\x01\xeb|\x07\xad\x01\xeb\x00\xf0\x20\xe3|g' "${2}"
+
+            # Before
+            # 53 b0 01 eb  # bl         send_EarsCompensation_dailyuse_to_xlog
+            # 56 b0 01 eb  # bl         send_EarsCompensation_to_xlog
+            # After
+            # 00 f0 20 e3  # nop
+            # 00 f0 20 e3  # nop
+            sed -i 's|\x53\xb0\x01\xeb\x56\xb0\x01\xeb|\x00\xf0\x20\xe3\x00\xf0\x20\xe3|g' "${2}"
+
+            # Before
+            # 04 10 a0 e1  # cpy        r1,r4
+            # 57 b0 01 eb  # bl         send_music_playback_to_xlog
+            # After
+            # 04 10 a0 e1  # cpy        r1,r4
+            # 00 f0 20 e3  # nop
+            sed -i 's|\x04\x10\xa0\xe1\x57\xb0\x01\xeb|\x04\x10\xa0\xe1\x00\xf0\x20\xe3|g' "${2}"
+
+            # Before
+            # 18 10 90 e5  # ldr        r1,[r0,#0x18]
+            # 58 b0 01 eb  # bl         send_misound_data_to_xlog
+            # After
+            # 18 10 90 e5  # ldr        r1,[r0,#0x18]
+            # 00 f0 20 e3  # nop
+            sed -i 's|\x18\x10\x90\xe5\x58\xb0\x01\xeb|\x18\x10\x90\xe5\x00\xf0\x20\xe3|g' "${2}"
             ;;
     esac
 }
 
-# Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
 
-extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+set -e
 
-"${MY_DIR}/setup-makefiles.sh"
+export DEVICE=cas
+export DEVICE_COMMON=sm8250-common
+export VENDOR=xiaomi
+
+"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
